992f8abf80727ef23f17b51aafc8657a2a2fe2d4
Kossuranta <kossuranta@gmail.com>
2026-02-01 12:16:25 +0200

    Implement wake up and sleep animations

--- Scripts/Bed.cs
+++ Scripts/Bed.cs
@@ -3,12 +3,45 @@ using Godot;
 public partial class Bed : StaticBody3D
 {
 	[Export] public BedArea Area { get; set; }
+	[Export] public AnimatedSprite3D WakeUpWarning { get; set; }
+	[Export] public AnimatedSprite3D SleepEffect { get; set; }
 
 	public override void _Ready()
 	{
 		if (Area == null)
 		{
 			GD.PrintErr("Bed: Area is not assigned! Please assign it in the editor.");
+		}
+
+		if (WakeUpWarning == null)
+		{
+			GD.PrintErr("Bed: WakeUpWarning is not assigned!");
+		}
+
+		if (SleepEffect == null)
+		{
+			GD.PrintErr("Bed: SleepEffect is not assigned!");
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		GameManager gm = GameManager.Instance;
+		if (gm == null)
+		{
+			return;
+		}
+
+		bool isSleepFilling = gm.CurrentEvent == MaskType.None || gm.HasCorrectMask();
+
+		if (WakeUpWarning != null)
+		{
+			WakeUpWarning.Visible = !isSleepFilling;
+		}
+
+		if (SleepEffect != null)
+		{
+			SleepEffect.Visible = isSleepFilling;
 		}
 	}
 }
